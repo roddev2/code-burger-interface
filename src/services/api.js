6a9b72ae1a30ebc13abd63dff1fe0f0452cbc337
rodolfomori <rod_dolpho@hotmail.com>
2023-05-27 20:45:16 -0300

    first commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiCodeBurger = axios.create({
-  baseURL: 'http://localhost:3001'
+  baseURL: 'https://api-code-burger-production-3f8e.up.railway.app/'
 })
 
 apiCodeBurger.interceptors.request.use(async config => {
